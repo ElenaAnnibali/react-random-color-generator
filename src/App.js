c2b919550511102de974d252ae9b2ca2ea960965
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2022-05-10 20:42:30 +0200

    Working basic version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import Div from './Div.js';
-import HueLuminosity from './HueLuminosity.js';
 
 export default function App() {
   return (
     <div className="App">
+      <h4>A modest version of</h4>
       <h1>Random Color Generator</h1>
       <br />
+      <h2>Click the cute button to generate a random color</h2>
       <br />
+      <be />
       <Div />
-      <br />
-      <br />
-      <h2>Choose the Hue and the Luminosity</h2>
-      <HueLuminosity />
       <br />
       <br />
       <br />
--- src/Div.js
+++ src/Div.js
@@ -1,29 +1,95 @@
+import './App.css';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import randomColor from 'randomcolor';
 import { useState } from 'react';
 
-const randomHexColor = () => {
-  // Generate a random 2 digit hex number, padded with a 0 if necessary
-  const part = () =>
-    Math.floor(Math.random() * 256)
-      .toString(16)
-      .padStart(2, '0');
-  const r = part();
-  const g = part();
-  const b = part();
-  return `#${r}${g}${b}`;
-};
-
-const myDivStyles = (color) => css`
-  background-color: ${color ? color : null};
+const myDivStyle = (generateColor) => css`
+  background-color: ${generateColor ? generateColor : null};
+  align-items: center;
+  transition-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  transition-duration: 1s;
+  box-sizing: border-box;
+  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr;
+  gap: 0px 0px;
+  grid-template-areas:
+    '. . .'
+    '. . .'
+    '. . .';
+  width: 20em;
+  height: 20em;
+  position: sticky;
+  top: 0;
+  left: 40%;
 `;
 
+const myButtonStyle = css`
+font-family: "Open Sans", sans-serif;
+font-size: 16px;
+letter-spacing: 2px;
+text-decoration: none;
+text-transform: uppercase;
+color: #000;
+cursor: pointer;
+border: 3px solid;
+padding: 0.25em 0.5em;
+box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;
+position: relative;
+user-select: none;
+-webkit-user-select: none;
+touch-action: manipulation;
+}`;
+
 export default function Div() {
-  const [color, setColor] = useState('');
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
+  const [generateColor, setGenerateColor] = useState(
+    randomColor.randomColor({ hue: hue, luminosity: luminosity }),
+  );
+
   return (
-    <div>
-      <div css={myDivStyles(color)}>Generated color: {color}</div>
-      <button onClick={() => setColor(randomHexColor())}>Generate</button>
-    </div>
+    <>
+      <button
+        onClick={() =>
+          setGenerateColor(
+            randomColor.randomColor({ hue: hue, luminosity: luminosity }),
+          )
+        }
+        css={myButtonStyle}
+      >
+        Generate
+      </button>
+      <br />
+      <br />
+      <h2>Otherwise choose the Hue and the Luminosity</h2>
+      <h4>and don't forget to click that cute button!</h4>
+      <br />
+      <label>
+        Hue:
+        <br />
+        <input
+          value={hue}
+          onChange={(event) => setHue(event.currentTarget.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Luminosity:
+        <br />
+        <input
+          value={luminosity}
+          onChange={(event) => setLuminosity(event.currentTarget.value)}
+        />
+      </label>
+      <br />
+      <br />
+      <br />
+      <div css={myDivStyle(generateColor)}>
+        Generated Color: {generateColor}
+      </div>
+    </>
   );
 }
