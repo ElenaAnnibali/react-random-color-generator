d9026ca293db3bdeccc64fab60dac98c9b555a41
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2022-05-10 09:21:05 +0200

    First draft of stretch goals - need to be changed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,44 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 const Div = styled.div`
+  color: 'white';
   background-color: ${(props) => props.bg};
   display: inline;
 `;
 
 export default function App() {
   const [color, setColor] = useState(randomColor());
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
 
   return (
     <div className="App">
-      Random Color Generator
+      <h1>Random Color Generator</h1>
       <br />
       <br />
       <button onClick={() => setColor(randomColor())}>Generate</button>
       <br />
       <br />
       <Div bg={color}>Generated Color: {color}</Div>
+      <br />
+      <br />
+      <div>Hue: {randomColor().hue}</div>
+      <input
+        value={hue}
+        onChange={(event) => setHue(event.currentTarget.value)}
+      />
+      <Div bg={hue}>Generated Color: {hue}</Div>
+      <div>Luminosity:</div>
+      <input
+        value={luminosity}
+        onChange={(event) => setLuminosity(event.currentTarget.value)}
+      />
+      <Div bg={luminosity}>Generated Color: {luminosity}</Div>
+      <br />
     </div>
   );
 }
+
+// {randomColor.luminosity('light' || 'dark')
+//        ? randomColor.luminosity
+//        : "Enter 'light' or 'dark'."}
