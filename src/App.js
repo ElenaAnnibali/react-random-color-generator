7498edbeb118b2ab30579da62b15e18a68d10607
ElenaAnnibali <77542450+ElenaAnnibali@users.noreply.github.com>
2022-08-02 09:54:13 -0400

    Fix styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.js';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import Div from './Div.js';
+
+const mainStyles = css`
+  min-height: 100vh;
+  padding-top: 20px;
+`;
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App" css={mainStyles}>
       <h4>A modest version of</h4>
       <h1>Random Color Generator</h1>
       <br />
